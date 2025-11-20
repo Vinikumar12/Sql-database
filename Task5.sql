b44d76d99190af1673f78ea841abe9740effa2c4
Vinikumar12 <159903427+Vinikumar12@users.noreply.github.com>
2025-11-20 20:58:57 +0530

    Add files via upload

---
+++ Task5.sql
@@ -0,0 +1,42 @@
+use batch61;
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Email VARCHAR(100),
+    City VARCHAR(50)
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE,
+    Amount DECIMAL(10,2),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+INSERT INTO Customers (CustomerID, Name, Email, City) VALUES
+(1, 'Alice', 'alice@example.com', 'Hyderabad'),
+(2, 'Bob', 'bob@example.com', 'Delhi'),
+(3, 'Charlie', 'charlie@example.com', 'Mumbai');
+
+INSERT INTO Orders (OrderID, CustomerID, OrderDate, Amount) VALUES
+(101, 1, '2025-11-01', 2500.00),
+(102, 2, '2025-11-05', 1800.00),
+(103, 1, '2025-11-10', 3200.00),
+(104, 4, '2025-11-15', 1500.00);
+
+SELECT Customers.CustomerID, Customers.Name, Orders.OrderID
+FROM Customers
+INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.CustomerID, Customers.Name, Orders.OrderID
+FROM Customers
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.CustomerID, Customers.Name, Orders.OrderID
+FROM Customers
+RIGHT JOIN Orders ON Customers.CustomerID = Orders.CustomerID;
+
+SELECT Customers.CustomerID, Customers.Name, Orders.OrderID
+FROM Customers
+FULL OUTER JOIN Orders ON Customers.CustomerID = Orders.CustomerID;
