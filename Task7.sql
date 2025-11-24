777dfec7d3fbb1e78eb881f95bf0dc4510dc272d
Vinikumar12 <159903427+Vinikumar12@users.noreply.github.com>
2025-11-24 21:08:37 +0530

    Update comments for view definitions in Task7.sql

--- Task7.sql
+++ Task7.sql
@@ -1,8 +1,10 @@
 /*View definitions and usage examples:
 
-CREATE VIEW with complex queries, demonstrate how they’re used, and show how they provide abstraction and security. The outcome is mastering the idea of reusable SQL logic.*/
+CREATE VIEW with complex queries, demonstrate how they’re used, and show how they provide abstraction and security. 
+The outcome is mastering the idea of reusable SQL logic.*/
 eg:
 CREATE VIEW EmployeeDept AS
 SELECT e.name, d.department_name
 FROM Employees e
 JOIN Departments d ON e.dept_id = d.id;
+
